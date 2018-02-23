bdf8b55160991177e5a974352666661cbcbf2d4d
ontheloosewu <35973933+ontheloosewu@users.noreply.github.com>
2018-02-23 13:33:41 -0500

    Uploaded script files for menus
    Just in case scripts are needed

---
+++ MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    private void Start()
+    {
+        Cursor.visible = true;
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Cursor.visible = false;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ PauseMenuUI.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuUI : MonoBehaviour {
+
+    public static bool isGamePaused = false;
+    public GameObject pauseMenuUI;
+	
+	// Update is called once per frame
+	void Update () {
+        if (!SceneManager.GetSceneByName("MainMenu").isLoaded)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (isGamePaused)
+                {
+                    Resume();
+                }
+                else
+                {
+                    Pause();
+                }
+            }
+        }
+	}
+
+    public void Resume()
+    {
+        Cursor.visible = false;
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isGamePaused = false;
+    }
+
+    public void Pause()
+    {
+        Cursor.visible = true;
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isGamePaused = true;
+    }
+
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1f;        //temp fix
+        pauseMenuUI.SetActive(false);   //temp fix
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
